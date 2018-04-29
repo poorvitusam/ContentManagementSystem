a3ba6640182b65e8ae1f427a1a94c86540f168b5
poorvitusam <poorvitusam@gmail.com>
2018-04-29 16:59:45 -0400

    Refactoring Code

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,84 +1,19 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-var flash = require('connect-flash');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
 
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/angularnodecms');
 
-var index = require('./routes/index');
-// var login = require('./routes/login');
-// var home = require('./routes/home');
-// var api = require('./routes/api');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// Body Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use(session ({
-	secret: 'secret',
-	saveUninitialized: true,
-	resave: true
-}));
-
-// Passport init
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-// require('./path/to/passport/config/file')(passport);
-
-// Express Validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
-
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
-
-// Connect Flash
-app.use(flash());
-
-// Global Vars
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  res.locals.user = req.user || null;
-  next();
-});
-
 
 app.use(function (req, res, next) {
 
@@ -106,29 +41,8 @@ var users = require('./routes/user.js');
 
 app.use('/pages', pages);
 app.use('/users', users);
-
-// app.use('/login', login); 
-// app.use('/api', api);
-// app.use('/admin', index);
 app.use('/',pages);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 var port = 3000;
 
--- nodeapp/routes/login.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var UserAdmin = require('../models/admin');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
-
-
-module.exports = router;
